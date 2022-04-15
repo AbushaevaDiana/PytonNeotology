e71c625c6d9d9222672286478710b986f1e72111
AbushaevaDiana <79981087+AbushaevaDiana@users.noreply.github.com>
2022-04-15 20:41:26 +0300

    Update todolist.py

--- todolist.py
+++ todolist.py
@@ -3,16 +3,21 @@ add - добавить задачу в список
 show - показать все добавленные задачи
 """
 todo = {}
-command = input("Введите команду: ")
 
-if command == "help":
-  print(HELP)
-elif command == "show":
-  print(todo)
-elif command == "add":
-  day = input("Введите дату: ")
-  task = input("Введите задачу: ")
-  todo[day] = task
-  print("Задача добавленна")
-else:
-  print("Неизвестная команда")
+
+endOfProgramm = False
+
+while not endOfProgramm:
+  command = input("Введите команду: ")
+  if command == "help":
+    print(HELP)
+  elif command == "show":
+    print(todo)
+  elif command == "add":
+    day = input("Введите дату: ")
+    task = input("Введите задачу: ")
+    todo[day] = task
+    print("Задача добавленна")
+  else:
+    print("Неизвестная команда")
+    break
